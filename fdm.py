72febc07dd1914b369c95a64c8619e60c9930654
Kajetan Janiak <kajetan.janiak@gmail.com>
2019-01-20 19:23:32 +0100

    sparse solvers and comments for FDM

--- fdm.py
+++ fdm.py
@@ -3,6 +3,21 @@ from scipy.sparse import diags, csr_matrix
 
 
 def fdm(b: Callable, sigma2: Callable, h: float, n: int) -> Tuple[csr_matrix, float]:
+    """Funkcja zwraca macierz (która pomnożona przez u daje u_t) i wartość specjalną.
+
+    Funkcja zwraca macierz A wymiaru n x n spełniającą
+    w przybliżeniu (metoda różnic skończonych)
+
+      Au = u_t = b(x)u_x + sigma^2(x)/2 u_xx,
+
+    oraz wartość specjalną, niezbędną w przypadku użycia schematu trapezów.
+
+    :param b: funkcja związana z adwekcją
+    :param sigma2: funkcja związana z dyfuzją
+    :param h: długość kroku
+    :param n: liczba kroków
+    :return:
+    """
     diagonal = [-3 / (2 * h)]
     under = []
     over1 = [2 / h]
@@ -12,6 +27,8 @@ def fdm(b: Callable, sigma2: Callable, h: float, n: int) -> Tuple[csr_matrix, fl
         b_k = b(h * k) / (2 * h)
         diagonal.append(-s_k)
         under.append((s_k / 2) - b_k)
+        # W ostatnim kroku do naddiagonali dodajemy dodatkowy element.
+        # Obetniemy go i zwrócimy jako wartość specjalną.
         over1.append((s_k / 2) + b_k)
 
     return diags((under, diagonal, over1[:-1], over2), (-1, 0, 1, 2)), over1[-1]
