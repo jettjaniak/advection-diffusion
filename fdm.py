f779829038ce5e2942653fe3849f6e0d5060b140
Kajetan Janiak <kajetan.janiak@gmail.com>
2019-01-20 18:38:50 +0100

    FDM schemes now without first row

--- fdm.py
+++ fdm.py
@@ -1,18 +1,17 @@
-from typing import Callable
+from typing import Callable, Tuple
 from scipy.sparse import diags, csr_matrix
 
 
-def fdm(b: Callable, sigma2: Callable, h: float, n: int) -> csr_matrix:
+def fdm(b: Callable, sigma2: Callable, h: float, n: int) -> Tuple[csr_matrix, float]:
     diagonal = [-3 / (2 * h)]
     under = []
     over1 = [2 / h]
-    over2 = [-1 / (2 * h)] + [0] * (n - 2)
+    over2 = [-1 / (2 * h)] + [0] * (n - 3)
     for k in range(1, n):
         s_k = sigma2(h * k) / (h ** 2)
         b_k = b(h * k) / (2 * h)
         diagonal.append(-s_k)
         under.append((s_k / 2) - b_k)
         over1.append((s_k / 2) + b_k)
-    under.append(0)
-    diagonal.append(1)
-    return diags((under, diagonal, over1, over2), (-1, 0, 1, 2))
+
+    return diags((under, diagonal, over1[:-1], over2), (-1, 0, 1, 2)), over1[-1]
--- solvers.py
+++ solvers.py
@@ -1,26 +1,34 @@
 import numpy as np
 
+from typing import Callable
 from scipy.linalg import solve as linear_solver
 
 
 def backward_euler(u_0: np.array, derivative_matrix: np.array, tau: float, m: int):
-    n = len(u_0) - 1
-    u = np.empty((m+1, n+1))
+    n = len(u_0)
+    u = np.empty((m+1, n))
     u[0] = u_0
-    u[1] = linear_solver(np.eye(n+1) - tau * derivative_matrix, u[0])
-    u[1, n] = 0
-    for i in range(2, m+1):
-        u[i] = linear_solver(np.eye(n+1) - tau * derivative_matrix, u[i-1])
+    for i in range(1, m+1):
+        u[i] = linear_solver(np.eye(n) - tau * derivative_matrix, u[i-1])
     return u
 
 
-def trapezoids(u_0: np.array, derivative_matrix: np.array, tau: float, m: int):
-    n = len(u_0) - 1
-    u = np.empty((m+1, n+1))
+def trapezoids(u_0: np.array, derivative_matrix: np.array, special: float,
+               tau: float, m: int):
+    n = len(u_0)
+    u = np.empty((m+1, n))
     u[0] = u_0
-    for i in range(1, m+1):
+
+    first_right_vector = (np.eye(n) + 0.5 * tau * derivative_matrix) @ u[0]
+    first_right_vector[-1] += 0.5 * tau * special
+    u[1] = linear_solver(
+        np.eye(n) - 0.5 * tau * derivative_matrix,
+        first_right_vector
+    )
+
+    for i in range(2, m+1):
         u[i] = linear_solver(
-            np.eye(n+1) - 0.5 * tau * derivative_matrix,
-            (np.eye(n + 1) + 0.5 * tau * derivative_matrix) @ u[i-1]
+            np.eye(n) - 0.5 * tau * derivative_matrix,
+            (np.eye(n) + 0.5 * tau * derivative_matrix) @ u[i-1]
         )
     return u
