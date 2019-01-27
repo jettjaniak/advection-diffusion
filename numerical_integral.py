fixed mistake in computation of expected value

numerical_integral.py
@@ -23,6 +23,33 @@ def make_kappa(b: Callable[[float], float], sigma2: Callable[[float], float])\
     return kappa
 
 
+def compute_psi_vals(linspace, params: Parameters):
+    """Liczy wartość oczekiwaną momentu zatrzymania.
+
+    :param linspace:
+    :param params: parametry modelu
+    :return:
+    """
+    n = len(linspace)
+    b = make_b(params)
+    sigma2 = make_sigma2(params)
+    kappa = make_kappa(b, sigma2)
+    kappa_pieces = np.array([0] + [kappa(linspace[i], linspace[i+1]) for i in range(n-1)])
+    kappa_vals = np.cumsum(kappa_pieces)
+    return np.exp(kappa_vals)
+
+
+def compute_eta_vals(linspace, psi_vals, sigma2):
+    """Liczy wartość oczekiwaną momentu zatrzymania.
+
+    :param linspace:
+    :param params: parametry modelu
+    :return:
+    """
+    eta_integrand_vals = psi_vals / sigma2(linspace)
+    return cumtrapz(eta_integrand_vals, linspace, initial=0)
+
+
 def compute_integral(step: float, n: int, params: Parameters) -> Tuple[np.ndarray, np.ndarray]:
     """Liczy wartość oczekiwaną momentu zatrzymania.
 
@@ -31,18 +58,13 @@ def compute_integral(step: float, n: int, params: Parameters) -> Tuple[np.ndarra
     :param params: parametry modelu
     :return:
     """
-    b = make_b(params)
     sigma2 = make_sigma2(params)
-    kappa = make_kappa(b, sigma2)
-    # Trick, żeby dostać zaokrąglonego inta od razu.
-    # Epsilon = h
-    #n_of_steps = int(0.5 + (params.n_star - step) / step)
+
     linspace = np.linspace(step, params.n_star, n)
-    kappa_pieces = np.array([0] + [kappa(linspace[i], linspace[i+1]) for i in range(n-1)])
-    psi_vals = np.exp(np.cumsum(kappa_pieces))
+    psi_vals = compute_psi_vals(linspace, params)
 
-    eta_integrand_vals = psi_vals/sigma2(linspace)
-    eta_vals = cumtrapz(eta_integrand_vals, linspace, initial=0)
-
-    tau_vals = 2*np.flipud(cumtrapz(eta_vals/psi_vals, linspace, initial=0))
+    eta_vals = compute_eta_vals(linspace, psi_vals, sigma2)
+    pre_tau_vals = cumtrapz(eta_vals/psi_vals, linspace, initial=0)
+    half_tau_vals_reversed = pre_tau_vals[-1] - np.flipud(pre_tau_vals)
+    tau_vals = 2*np.flipud(half_tau_vals_reversed)
     return linspace, tau_vals